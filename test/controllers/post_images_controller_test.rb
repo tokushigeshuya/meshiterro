98b9b617ac3337d7e773e7322b3b2361b5acd056
EC2 Default User <ec2-user@ip-172-31-5-25.ec2.internal>
2022-02-17 12:40:03 +0000

    [finish]4-16

---
+++ test/controllers/post_images_controller_test.rb
@@ -0,0 +1,13 @@
+require "test_helper"
+
+class PostImagesControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get post_images_index_url
+    assert_response :success
+  end
+
+  test "should get show" do
+    get post_images_show_url
+    assert_response :success
+  end
+end
